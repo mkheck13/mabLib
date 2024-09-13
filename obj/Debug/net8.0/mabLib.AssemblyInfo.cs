3208722440f35e8bbe691bb04c236f95747af4aa
mkheck13 <mkheck13@gmail.com>
2024-09-12 17:10:27 -0700

    update

--- obj/Debug/net8.0/mabLib.AssemblyInfo.cs
+++ obj/Debug/net8.0/mabLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mabLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32922fef25d2a04104f7d1f7c19b7158763bfe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73024ad2fa936f3e655167cefee476db073be3d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("mabLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mabLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
